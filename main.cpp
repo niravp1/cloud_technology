36ddcbb7175f325d2efa8db57adff3805180f77e
niravp1 <143545148+niravp1@users.noreply.github.com>
2024-09-05 14:30:31 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,y;
+	cout << "Enter a value for x ";
+	cout << endl;
+	cin>>  x;
+	cout << "Enter a value for y ";
+	cin >> y;
+	cout << "The sum of is " << x + y <<"."
+	     << "The difference of is " << x - y <<"."
+	     << "The average is " <<
+
+
+	     cout << endl;
+
+	cout << x << ' ' << y;
+	return 0;
+}
